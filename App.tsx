6b1b60847f2f74feac549aa54e600da4a27ecbf4
Aandi1989 <navaliny1989@gmail.com>
2023-09-07 19:22:10 +0300

    add tailwind -first way

--- App.tsx
+++ App.tsx
@@ -1,24 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, TouchableOpacity, View } from "react-native";
+import tw from "twrnc";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{color:'green'}}>Open up App.tsx to start working on your app!</Text>
-      <Text style={{color:'green'}}>Open up App.tsx to start working on your app!</Text>
-      <Text style={{color:'green'}}>Open up App.tsx to start working on your app!</Text>
-      <Text style={{color:'green'}}>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`flex-1 justify-center items-center bg-black`}>
+      <TouchableOpacity style={tw`bg-teal-500 p-3 rounded-lg`}>
+        <Text style={tw`text-white text-3xl font-bold `}>Hello world!</Text>
+      </TouchableOpacity>
+      <StatusBar style="light" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
