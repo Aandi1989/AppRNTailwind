5452f1df9d54dc64e5f60d2ba5c4b4def77140dc
Aandi1989 <navaliny1989@gmail.com>
2023-09-08 11:48:57 +0300

    add tailwind -second way

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, TouchableOpacity, View } from "react-native";
-import tw from "twrnc";
 
 export default function App() {
   return (
-    <View style={tw`flex-1 justify-center items-center bg-black`}>
-      <TouchableOpacity style={tw`bg-teal-500 p-3 rounded-lg`}>
-        <Text style={tw`text-white text-3xl font-bold `}>Hello world!</Text>
+    <View className="flex-1 justify-center items-center bg-black">
+      <TouchableOpacity className="bg-teal-500 p-3 rounded-lg">
+        <Text className="text-white text-3xl font-bold ">Hello world!</Text>
       </TouchableOpacity>
       <StatusBar style="light" />
     </View>
